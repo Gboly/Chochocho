3469e88be248d2c413ffd67f6f580617816c0d76
gboly <gbolahanahmed1@gmail.com>
2022-12-22 16:37:53 +0100

    feat: Add more user backend routes
    In this commit, i have a created two routes, they are;
    - getUser route
        Here, a particular user is generated when the frontend passes the
        user Id as a parameter in the "/user" route
    - getUsersById route
        A list of users are expected to be generated from the database. The
        Ids of these users are passed or the ids of users that shouldn't be
        included is passed. This route handles the situation by figuring
        which result is expected and then returns it as a json.
    
    Author - Ahmed Quamordeen Gbolahan <gbolahanahmed1@gmail.com>

--- server/controllers/user/user.js
+++ server/controllers/user/user.js
@@ -1,5 +1,50 @@
+import User from "../../models/user.js";
+
 const getAuthenticatedUser = async (req, res) => {
   res.status(200).json(req.user);
 };
 
-export { getAuthenticatedUser };
+const getUser = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await User.findById(id, { password: 0 });
+    user
+      ? res.status(200).json(user)
+      : res.status(404).json({ error: "User not found" });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(400)
+      .json({ error: "An error was encountered. Incorrect details." });
+  }
+};
+
+const getUsersById = async (req, res) => {
+  const { id, id_ne } = req.query;
+  try {
+    const users = await fetchUsers(id, id_ne);
+    users.length > 0
+      ? res.status(200).json(users)
+      : res.status(404).json({ error: "No user found" });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(400)
+      .json({ error: "An error was encountered. Incorrect details." });
+  }
+};
+
+// Other functions
+const fetchUsers = async (id, id_ne) => {
+  const users = await User.find(
+    {
+      _id:
+        id_ne && id_ne.length > 0 ? { $ne: id_ne } : id && id.length > 0 && id,
+    },
+    { password: 0 }
+  );
+
+  return users;
+};
+
+export { getAuthenticatedUser, getUser, getUsersById };
--- server/routes/user/user.js
+++ server/routes/user/user.js
@@ -1,8 +1,14 @@
 import { Router } from "express";
-import { getAuthenticatedUser } from "../../controllers/user/user.js";
+import {
+  getAuthenticatedUser,
+  getUser,
+  getUsersById,
+} from "../../controllers/user/user.js";
 
 const router = Router();
 
 router.get("/authenticatedUser", getAuthenticatedUser);
+router.get("/:id", getUser);
+router.get("/", getUsersById);
 
 export default router;
