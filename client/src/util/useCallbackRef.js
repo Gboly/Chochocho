f13f737849f03d23242f442b8bf700c45ef93861
Gboly <gbolahanahmed1@gmail.com>
2023-07-10 21:56:02 +0100

    fix: Improve UI of comment page
    The offset of the "active comment" was 68px just so a part of the parent
    post is seen to show the user that there are parents. I realized this
    was not necessary as the scroll bar does the job of informing the user.
    The offset is reduced to 60px.
    
    Author - Ahmed Quamordeen Gbolahan <gbolahanahmed1@gmail.com>

--- client/src/util/useCallbackRef.js
+++ client/src/util/useCallbackRef.js
@@ -6,8 +6,8 @@ export default function useOffsetTop(postId) {
   const handleRef = React.useCallback(
     (node) => {
       // using setTimeout here to deal with the issue of stale values. The offsetTop keeps rendering previous value. There's a waiting period for this to be sorted
-      //The 68px represents the "allowed" offset.
-      node && setTimeout(() => setDefaultScrollTop(node.offsetTop - 68), 200);
+      //The 60px represents the "allowed" offset.
+      node && setTimeout(() => setDefaultScrollTop(node.offsetTop - 60), 200);
     },
     // Even if postId isnt a part of the this function. It is needed as a dependency because this page is mostly nested and this callback needs to be re-rendered everytime we view a new post(comment).
     [postId]
