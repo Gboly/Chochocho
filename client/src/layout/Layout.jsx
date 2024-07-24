cf84a2a9b4cb39d3f2bde283c916e0d232d96837
Gboly <gbolahanahmed1@gmail.com>
2024-07-24 09:46:56 +0100

    fix: Return Layout css file
    While cleaning up the Layout.jsx file, the layout css file was
    mistakenly removed. This commit returns it.

--- client/src/layout/Layout.jsx
+++ client/src/layout/Layout.jsx
@@ -1,6 +1,7 @@
 import Header from "./header/Header";
 import { Outlet } from "react-router-dom";
 import Sidebar from "./sidebar/Sidebar";
+import "./layout.css";
 
 export default function Layout() {
   return (
